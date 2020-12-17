f90dbe8b41c722d103cad11c78bac471a3930847
TruongVyPhat <63778351+TruongVyPhat@users.noreply.github.com>
2020-12-17 14:14:52 +0700

    testing process: 3 passed & 3 failed

--- contracts/TimeLockedWalletFactory.sol
+++ contracts/TimeLockedWalletFactory.sol
@@ -21,18 +21,18 @@ contract TimeLockedWalletFactory {
     {
         // Create new wallet.
         wallet = address(new TimeLockedWallet(msg.sender, _owner, _unlockDate));
-        address payable walletcv = address(uint160(wallet));
+        address payable converted_wallet = address(uint160(wallet));
         
         // Add wallet to sender's wallets.
-        wallets[msg.sender].push(walletcv);
+        wallets[msg.sender].push(converted_wallet);
 
         // If owner is the same as sender then add wallet to sender's wallets too.
         if(msg.sender != _owner){
-            wallets[_owner].push(walletcv);
+            wallets[_owner].push(converted_wallet);
         }
 
         // Send ether from this transaction to the created contract.
-        walletcv.transfer(msg.value);
+        converted_wallet.transfer(msg.value);
 
         // Emit event.
         emit Created(wallet, msg.sender, _owner, now, _unlockDate, msg.value);
